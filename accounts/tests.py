ddaaf4af4c7de6d6e0e4531f8c951628638fd155
Ashu202023 <ashutosh20kumar23@gmail.com>
2025-08-07 15:47:41 +0530

    user authentication

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model 
+from django.urls import reverse
 
 class UserTests(TestCase):
     def test_create_user(self):
@@ -26,6 +27,33 @@ class UserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpPageTests(TestCase):
+    def test_url_exists_at_right_location(self):
+        response=self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code,200)
+    
+    def test_signup_viewname(self):
+        response=self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"registration/signup.html")
+    
+    def test_signup_form(self):
+        response=self.client.post(
+            reverse("signup"),{
+                "username":"newuser",
+                "email":"newuser@email.com",
+                "password1":"newpass123",
+                "password2":"newpass123",
+
+            },
+        )
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(get_user_model().objects.all().count(),1)
+        self.assertEqual(get_user_model().objects.all()[0].username,"newuser")
+        self.assertEqual(get_user_model().objects.all()[0].email,"newuser@email.com")
+
 
 
 
